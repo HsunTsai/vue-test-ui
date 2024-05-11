a127f3e5f4312fb77e85b88cc28317dd4784e873
Hsun Tsai <hsuntsai0906@gmail.com>
2024-05-11 14:36:23 +0800

    fix component path

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+// import Header from './components/header/Header.vue'
+import Button from './components/button/Button.vue'
+// import Page from './components/page/Page.vue'
+
+// 导出你的组件
+export {
+    // Header,
+    Button,
+    // Page,
+}
